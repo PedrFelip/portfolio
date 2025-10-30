d0a4c114f8aaee40d5d2729877e6cc97e9718790
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-30 13:32:48 -0300

    feat: refactor speed insights integration in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,12 @@
 	import Navbar from '$lib/components/portfolio/Navbar.svelte';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
-	import { SpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	
+	injectSpeedInsights();
 </script>
 
 <ModeWatcher defaultMode="dark" />
-<SpeedInsights />
 <div class="relative mx-auto min-h-screen max-w-2xl bg-background px-4 sm:px-6 py-12 sm:py-24 font-sans antialiased">
 	<slot></slot>
 	<Navbar />

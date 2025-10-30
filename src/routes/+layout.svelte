b7950c7afa1606a1c81738fc3ebf86cd30af5201
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-30 13:31:44 -0300

    fix: set default mode for ModeWatcher in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script>
 	import Navbar from '$lib/components/portfolio/Navbar.svelte';
 	import '../app.css';
-	import { ModeWatcher, setMode } from 'mode-watcher';
+	import { ModeWatcher } from 'mode-watcher';
 	import { SpeedInsights } from '@vercel/speed-insights/sveltekit';
-	setMode('dark');
 </script>
 
-<ModeWatcher />
+<ModeWatcher defaultMode="dark" />
 <SpeedInsights />
 <div class="relative mx-auto min-h-screen max-w-2xl bg-background px-4 sm:px-6 py-12 sm:py-24 font-sans antialiased">
 	<slot></slot>

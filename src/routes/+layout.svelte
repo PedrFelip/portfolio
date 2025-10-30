a4d3938243da7b7f7a93faa635ddbe0d073d4902
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-30 13:28:34 -0300

    feat: add @vercel/speed-insights dependency and integrate into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
 	import Navbar from '$lib/components/portfolio/Navbar.svelte';
 	import '../app.css';
 	import { ModeWatcher, setMode } from 'mode-watcher';
+	import { SpeedInsights } from '@vercel/speed-insights/sveltekit';
 	setMode('dark');
 </script>
 
 <ModeWatcher />
+<SpeedInsights />
 <div class="relative mx-auto min-h-screen max-w-2xl bg-background px-4 sm:px-6 py-12 sm:py-24 font-sans antialiased">
 	<slot></slot>
 	<Navbar />

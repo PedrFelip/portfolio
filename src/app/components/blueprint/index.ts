e7722ec192156f5d4c9726431f19cbbda277ce71
Pedro Felipe <pfsilva190406@gmail.com>
2026-03-20 11:40:03 -0300

    feat(blueprint): export FooterGrid and FooterGridCell

--- src/app/components/blueprint/index.ts
+++ src/app/components/blueprint/index.ts
@@ -1,5 +1,6 @@
 export { CornerBracket, CornerBrackets } from "./CornerBracket";
 export { DotPattern } from "./DotPattern";
+export { FooterGrid, FooterGridCell } from "./FooterGrid";
 export { RailBounded } from "./RailBounded";
 export { RailLayout } from "./RailLayout";
 export { SectionDivider } from "./SectionDivider";
